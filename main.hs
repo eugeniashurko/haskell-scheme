f2938bde0486114c17e5ccbb47a32abc738b1fff
Eugenia <yarutoua@gmail.com>
2014-05-03 18:48:30 +0300

    Primitives evaluation added

--- main.hs
+++ main.hs
@@ -164,18 +164,63 @@ showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum _          = 0
+
+primitives :: [(String , [LispVal] -> LispVal)]
+primitives = [("+" , numericBinop (+)) ,
+              ("-" , numericBinop (-)) ,
+              ("*" , numericBinop (*)) ,
+              ("/" , numericBinop div) ,
+              ("mod" , numericBinop mod) ,
+              ("quotient" , numericBinop quot) ,
+              ("remainder" , numericBinop rem) ,
+              ("symbol?" , unaryOp symbolp) ,
+              ("string?" , unaryOp stringp) ,
+              ("number?" , unaryOp numberp) ,
+              ("bool?", unaryOp boolp) ,
+              ("list?" , unaryOp listp)]
+
+unaryOp :: (LispVal -> LispVal) -> [LispVal] -> LispVal
+unaryOp f [v] = f v
+
+symbolp, numberp, stringp, boolp, listp :: LispVal -> LispVal
+symbolp (Atom _)   = Bool True
+symbolp _          = Bool False
+numberp (Number _) = Bool True
+numberp _          = Bool False
+stringp (String _) = Bool True
+stringp _          = Bool False
+boolp   (Bool _)   = Bool True
+boolp   _          = Bool False
+listp   (List _)   = Bool True
+listp   (DottedList _ _) = Bool True
+listp   _          = Bool False
+
+symbol2string, string2symbol :: LispVal -> LispVal
+symbol2string (Atom s)   = String s
+symbol2string _          = String ""
+string2symbol (String s) = Atom s
+string2symbol _          = Atom ""
+
 eval :: LispVal -> LispVal
 eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
 
-readExpr :: String -> String
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found: " ++ show val
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
 
 main :: IO ()
-main = do 
-    args <- getArgs
-    putStrLn (readExpr (args !! 0))
+main = getArgs >>= print . eval . readExpr . head
